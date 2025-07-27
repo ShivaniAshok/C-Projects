ba6b6fccd947eced8aaa14ae3e7f36317546036d
ShivaniAshok <shivaniashok34@gmail.com>
2025-07-27 23:07:22 +0530

    added simple calculator project

---
+++ Simple Calculator/calculator.c
@@ -0,0 +1,82 @@
+/*
+below commands to execute program:
+
+gcc calculator.c
+a
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void print_menu();
+
+int main()
+{
+    int choice;
+    double num1, num2, res = 0;
+
+    print_menu();
+    printf("\nEnter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > 7)
+    {
+        printf("\nPlease enter a valid choice (1-7).\n");
+        return 1;
+    }
+
+    if (choice == 7)
+    {
+        printf("Exiting calculator...\n");
+        return 0;
+    }
+
+    printf("\nPlease enter the first num: ");
+    scanf("%lf", &num1);
+
+    printf("Now enter the second num: ");
+    scanf("%lf", &num2);
+
+    switch (choice)
+    {
+    case 1:
+        res = num1 + num2;
+        break;
+    case 2:
+        res = num1 - num2;
+        break;
+    case 3:
+        res = num1 * num2;
+        break;
+    case 4:
+        res = num1 / num2;
+        break;
+    case 5:
+        res = (int)num1 % (int)num2; // since num1 & num2 are double cast to int
+        break;
+    case 6:
+        res = pow(num1, num2);
+        break;
+    }
+
+    if (choice >= 1 && choice <= 6)
+    {
+        printf("\nResult of operation is: %.2lf", res);
+    }
+
+    return 0;
+}
+
+void print_menu()
+{
+    printf("\nWelcome to Simple Calculator\n");
+    printf("\nChoose one of the following options: ");
+    printf("\n1. Addition");
+    printf("\n2. Subtraction");
+    printf("\n3. Multiplication");
+    printf("\n4. Division");
+    printf("\n5. Modulus");
+    printf("\n6. Power");
+    printf("\n7. Exit");
+}
